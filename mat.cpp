2c3ab223d497c75419eedb71ace2b69ed18c9059
AmirGill <amirg00@walla.com>
2022-03-09 15:22:26 +0200

    Update date and author comment and requested files for this part.

---
+++ mat.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "mat.hpp"
+
+using namespace std;
+
+string ariel::mat(int COL, int ROW, char symbol_1, char symbol_2) {return "GG";}
---
+++ mat.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Amir on 08/03/2022.
+//
+
+#ifndef UNTITLED4_MAT_HPP
+#define UNTITLED4_MAT_HPP
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace ariel {
+    string mat(int, int, char, char);
+}
+
+#endif //UNTITLED4_MAT_HPP
